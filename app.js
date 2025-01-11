6935ed8c453cf4d7823085adc543fced0704619b
singba12 <145152665+singba12@users.noreply.github.com>
2025-01-11 18:02:29 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(bodyParser.json());
