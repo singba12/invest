5d835398871a436518640c49333a73183f13f7c3
singba12 <145152665+singba12@users.noreply.github.com>
2025-01-11 18:03:04 +0000

    Update script.js

--- public/script.js
+++ public/script.js
@@ -7,7 +7,7 @@ document.getElementById('calcForm').addEventListener('submit', async function(ev
     const endDate = document.getElementById('endDate').value;
 
     try {
-        const response = await fetch('http://localhost:3000/calculate', {
+        const response = await fetch('http://localhost:10000/calculate', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ symbol, dailyInvestment, startDate, endDate })
